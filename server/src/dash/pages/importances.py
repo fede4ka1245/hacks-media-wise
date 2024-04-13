5d44c47837fe8d8d3f31440649cdc9029ed7cfe9
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2024-04-13 12:59:36 +0300

    minor fixes of features plot handler

--- server/src/dash/pages/importances.py
+++ server/src/dash/pages/importances.py
@@ -4,20 +4,20 @@ from dash import html, dcc
 import plotly.express as px
 from pandas import read_json
 
+from re import compile as compile_regexp
+
 from server.src.monogodb import mongodb_collection
 
 
-dash.register_page(__name__, path_template="/<report_id>/features_plots/<feature_number>")
+dash.register_page(__name__, path_template="/<report_id>/importances_plot")
 
 
-def layout(report_id=None, feature_number=None, **kwargs):
+def layout(report_id=None, **kwargs):
     report = mongodb_collection.find_one({"_id": ObjectId(report_id)})
 
     if report is None:
         return html.Div(["Report not found"])
     else:
         df = read_json(report["data"])
-        feature_name = df.columns[int(feature_number)]
-        fig = px.line(df[["Пеиод__Начало нед", feature_name]], x="Пеиод__Начало нед", y=feature_name)
 
         return dcc.Graph(figure=fig)
