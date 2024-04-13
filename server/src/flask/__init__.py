a7b1f8408c6d5afe4bb56cec531e87fef65f6ff3
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2024-04-13 16:12:59 +0300

    fixed paths to plots handlers

--- server/src/flask/__init__.py
+++ server/src/flask/__init__.py
@@ -51,10 +51,10 @@ def get_results(report_id: str):
     df = read_json(report["data"])
     chart_items = list()
     for feature_number, feature_name in enumerate(df.columns[3:], start=3):
-        chart_item = {"feature": feature_name, "chart_link": f"/{report_id}/features_plots/{feature_number}"}
+        chart_item = {"feature": feature_name, "chart_link": f"api/{report_id}/features_plots/{feature_number}"}
         chart_items.append(chart_item)
 
-    feature_weights_chart_link = f"/{report_id}/importances"
+    feature_weights_chart_link = f"api/{report_id}/importances"
 
     return {
         "feature_weights_chart_link": feature_weights_chart_link,
