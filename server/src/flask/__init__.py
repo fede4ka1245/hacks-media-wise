35939233362409b3f2ea8c0b2ba5b768d9e9d7ec
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2024-04-13 12:48:07 +0300

    added status handler

--- server/src/flask/__init__.py
+++ server/src/flask/__init__.py
@@ -17,6 +17,7 @@ def upload():
     if file and file.filename != "":
         data, ml_data, columns_with_not_enough_information, mean_std_info = preprocess(file)
         insertion = mongodb_collection.insert_one({
+            "status": "processing",
             "data": data.to_json(),
             "ml_data": DataFrame(ml_data).to_json(),
             "excluded_columns": columns_with_not_enough_information,
@@ -27,18 +28,28 @@ def upload():
         return "smth didn't happen"
 
 
+@flask_server.route("/upload/<report_id>/status", methods=["GET", "POST"])
+def get_status(report_id: str):
+    report = mongodb_collection.find_one({"_id": ObjectId(report_id)})
+    if report is None:
+        return "no report found", 404
+
+    return report["status"]
+
+
 @flask_server.route("/upload/<report_id>/result", methods=["GET", "POST"])
 def get_results(report_id: str):
     report = mongodb_collection.find_one({"_id": ObjectId(report_id)})
     if report is None:
         return "no report found", 404
 
-    feature_weights_chart_link = "/no_way"
     df = read_json(report["data"])
     chart_items = list()
     for feature_number, feature_name in enumerate(df.columns[3:], start=3):
         chart_item = {"feature": feature_name, "chart_link": f"/{report_id}/features_plots/{feature_number}"}
         chart_items.append(chart_item)
+
+    feature_weights_chart_link = "/no_way"
 
     return {
         "feature_weights_chart_link": feature_weights_chart_link,
