580d15a6a0a6231896fc21abc554612f8f772b16
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2024-04-13 16:44:30 +0300

    plotting changes

--- server/src/dash/pages/features.py
+++ server/src/dash/pages/features.py
@@ -18,6 +18,7 @@ def layout(report_id=None, feature_number=None, **kwargs):
     else:
         df = read_json(report["data"])
         predicted_df = read_json(report["predicted_data"])
+
         df["predicted"] = 0
         predicted_df["predicted"] = 1
         df.set_index("Пеиод__Начало нед", inplace=True)
@@ -25,8 +26,11 @@ def layout(report_id=None, feature_number=None, **kwargs):
 
         df.update(predicted_df)
         df.reset_index(inplace=True)
+        predicted_df.reset_index(inplace=True)
 
         feature_name = df.columns[int(feature_number)]
-        fig = px.line(df[["Пеиод__Начало нед", feature_name]], x="Пеиод__Начало нед", y=feature_name)
+
+        fig = px.line(df[["Пеиод__Начало нед", feature_name, "predicted"]], x="Пеиод__Начало нед", y=feature_name, color="predicted", line_shape="spline", markers=True)
+        fig.add_vline(x=predicted_df["Пеиод__Начало нед"][0], annotation_text="Пронгозируемая дата", line_dash="dot")
 
         return dcc.Graph(figure=fig)
