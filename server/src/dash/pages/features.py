33db135ce0a785e68971f64d88e3aefad29febbc
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2024-04-13 12:56:30 +0300

    simplified features plot handler

--- server/src/dash/pages/features.py
+++ server/src/dash/pages/features.py
@@ -16,23 +16,12 @@ dash.register_page(__name__, path_template="/<report_id>/features_plots/<feature
 
 def layout(report_id=None, feature_number=None, **kwargs):
     report = mongodb_collection.find_one({"_id": ObjectId(report_id)})
+
     if report is None:
         return html.Div(["Report not found"])
     else:
-        return html.Div([
-            dcc.Graph(id="graph-content"),
-            dcc.Location(id="url", refresh=False),
-        ])
+        df = read_json(report["data"])
+        feature_name = df.columns[int(feature_number)]
+        fig = px.line(df[["Пеиод__Начало нед", feature_name]], x="Пеиод__Начало нед", y=feature_name)
 
-
-@callback(
-    Output("graph-content", "figure"),
-    Input("url", "pathname")
-)
-def update_graph(pathname: str):
-    report_id, feature_number_str = PAGE_TEMPLATE_REGEXP.match(pathname).groups()
-    feature_number = int(feature_number_str)
-    report = mongodb_collection.find_one({"_id": ObjectId(report_id)})
-    df = read_json(report["data"])
-    feature_name = df.columns[feature_number]
-    return px.line(df[["Пеиод__Начало нед", feature_name]], x="Пеиод__Начало нед", y=feature_name)
+        return dcc.Graph(figure=fig),
