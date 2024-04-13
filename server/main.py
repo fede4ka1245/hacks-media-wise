1adb2e538e8f278daf2e07c56b117b74e40dc9f4
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2024-04-13 14:44:05 +0300

    made setup options, updated requirements.txt

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
+from os import getenv
+
 from src.dash import dash_app
 
 if __name__ == '__main__':
-    dash_app.run(debug=True)
+    dash_app.run(debug=(getenv("DEBUG") == "TRUE"), port=int(getenv("PORT")), host="0.0.0.0")
