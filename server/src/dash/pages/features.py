514a9bbf3b6aacb8d0f34dca7b4164e3a949baef
bobrnebobr <mishabobrov291@gmail.com>
2024-04-13 17:05:06 +0300

    addmissing data rects

--- server/src/dash/pages/features.py
+++ server/src/dash/pages/features.py
@@ -1,11 +1,11 @@
 import dash
+import pandas as pd
 from bson import ObjectId
 from dash import html, dcc
 import plotly.express as px
 from pandas import read_json
 
 from src.monogodb import mongodb_collection
-
 
 dash.register_page(__name__, path_template="/api/<report_id>/features_plots/<feature_number>")
 
@@ -19,6 +19,7 @@ def layout(report_id=None, feature_number=None, **kwargs):
         df = read_json(report["data"])
         predicted_df = read_json(report["predicted_data"])
 
+        datetime_index = "Пеиод__Начало нед"
         df["predicted"] = 0
         predicted_df["predicted"] = 1
         df.set_index("Пеиод__Начало нед", inplace=True)
@@ -30,8 +31,23 @@ def layout(report_id=None, feature_number=None, **kwargs):
 
         feature_name = df.columns[int(feature_number)]
 
-        fig = px.line(df[["Пеиод__Начало нед", feature_name, "predicted"]], x="Пеиод__Начало нед", y=feature_name, color="predicted", line_shape="spline", markers=True)
-        fig.add_vline(x=predicted_df["Пеиод__Начало нед"][0], annotation_text="Пронгозируемая дата", line_dash="dot")
-        fig.update_xaxes(type="date", range=[df["Пеиод__Начало нед"].min(), predicted_df["Пеиод__Начало нед"].max()])
+        fig = px.line(df[[datetime_index, feature_name, "predicted"]], x=datetime_index, y=feature_name,
+                      color="predicted", line_shape="spline", markers=True)
+        fig.add_vline(x=predicted_df[datetime_index][0], annotation_text="Пронгозируемая дата", line_dash="dot")
+        fig.update_xaxes(type="date", range=[df[datetime_index].min(), predicted_df[datetime_index].max()])
+
+        missing_data_intervals = []
+        for i, value in enumerate(df[feature_name]):
+            if pd.isna(value):
+                if not missing_data_intervals:
+                    missing_data_intervals.append([i, i])
+                elif missing_data_intervals[-1][1] + 1 < i:
+                    missing_data_intervals.append([i, i])
+                else:
+                    missing_data_intervals[-1][1] += 1
+
+        for interval in missing_data_intervals:
+            fig.add_vrect(x0=df[datetime_index][interval[0]], x1=df[datetime_index][interval[1]], row="all", col=1,
+                          fillcolor="red", opacity=0.25, line_width=0)
 
         return dcc.Graph(figure=fig)
